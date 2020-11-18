0f3e74154632398cd10df9b852a724e613a9e850
Leonardo Losoviz <leo@getpop.org>
2020-11-18 19:00:28 +0800

    Initial commit

---
+++ src/MutationResolverBridges/CreateEventMutationResolverBridge.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSitesWassup\EventMutations\MutationResolverBridges;
+
+use PoPSitesWassup\EventMutations\MutationResolvers\CreateEventMutationResolver;
+
+class CreateEventMutationResolverBridge extends AbstractCreateUpdateEventMutationResolverBridge
+{
+    public function getMutationResolverClass(): string
+    {
+        return CreateEventMutationResolver::class;
+    }
+}
---
+++ src/MutationResolvers/AbstractCreateUpdateEventMutationResolver.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSitesWassup\EventMutations\MutationResolvers;
+
+use PoPSitesWassup\CustomPostMutations\MutationResolvers\AbstractCreateUpdateCustomPostMutationResolver;
+
+abstract class AbstractCreateUpdateEventMutationResolver extends AbstractCreateUpdateCustomPostMutationResolver
+{
+}
